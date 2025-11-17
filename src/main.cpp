8d8e6fe3faa22ed1943c68887deabe0a5395fbf0
Louis Vaillant <louis.vaillant@eleves.ec-nantes.fr>
2025-11-17 16:58:22 +0100

    [Dev] Add tests for Point class

--- src/main.cpp
+++ src/main.cpp
@@ -1,7 +1,17 @@
 #include <iostream>
 
+#include "Point.hpp"
+
 int main(int argc, char const *argv[])
 {
-    std::cout << "Hello, World!" << std::endl;
+    Point<int> piti_point(5, 8);
+    std::cout << piti_point << std::endl;
+
+    Point<int> piti_point_le_deuxieme(piti_point);
+    std::cout << piti_point_le_deuxieme << std::endl;
+
+    piti_point_le_deuxieme.translate(-5, 2);
+    std::cout << piti_point_le_deuxieme << std::endl;
+
     return 0;
 }
