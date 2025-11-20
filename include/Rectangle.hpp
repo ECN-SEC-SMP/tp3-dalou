c367f5c1879b61b8f36c8e6d1319cf1a34996bba
Louis Vaillant <louis.vaillant@eleves.ec-nantes.fr>
2025-11-20 13:30:24 +0100

    [Fix] (Rectangle) (Square) Remove useless typename
    Co-authored-by: damien-br <damien-br@users.noreply.github.com>

--- include/Rectangle.hpp
+++ include/Rectangle.hpp
@@ -2,63 +2,63 @@
 
 #include "Forme.hpp"
 
-template <typename T, typename T_point = T, typename T_compute = T_point>
+template <typename T_point, typename T_compute = T_point>
 class Rectangle;
 
-template <typename T, typename T_point = T, typename T_compute = T_point>
-std::ostream &operator<<(std::ostream &cout, Rectangle<T, T_point, T_compute> const& rectangle);
+template <typename T_point, typename T_compute = T_point>
+std::ostream &operator<<(std::ostream &cout, Rectangle<T_point, T_compute> const& rectangle);
 
-template <typename T, typename T_point, typename T_compute>
+template <typename T_point, typename T_compute>
 class Rectangle : public Forme<T_point, T_compute>
 {
 private:
-    T width, height;
+    T_compute width, height;
 public:
-    Rectangle(T width, T height, T_point x, T_point y);
+    Rectangle(T_compute width, T_compute height, T_point x, T_point y);
     ~Rectangle();
 
-    T getWidth() const;
-    T getHeight() const;
+    T_compute getWidth() const;
+    T_compute getHeight() const;
 
     virtual T_compute perimeter();
     virtual T_compute surface();
 
-    friend std::ostream &operator<< <T, T_point, T_compute>(std::ostream &cout, const Rectangle &rectangle);
+    friend std::ostream &operator<< <T_point, T_compute>(std::ostream &cout, const Rectangle &rectangle);
 };
 
-template <typename T, typename T_point, typename T_compute>
-Rectangle<T, T_point, T_compute>::Rectangle(T width, T height, T_point x, T_point y)
+template <typename T_point, typename T_compute>
+Rectangle<T_point, T_compute>::Rectangle(T_compute width, T_compute height, T_point x, T_point y)
 : Forme<T_point, T_compute>(Point<T_point>(x, y)), width(width), height(height) {}
 
-template <typename T, typename T_point, typename T_compute>
-Rectangle<T, T_point, T_compute>::~Rectangle() {}
+template <typename T_point, typename T_compute>
+Rectangle<T_point, T_compute>::~Rectangle() {}
 
-template <typename T, typename T_point, typename T_compute>
-T Rectangle<T, T_point, T_compute>::getWidth() const
+template <typename T_point, typename T_compute>
+T_compute Rectangle<T_point, T_compute>::getWidth() const
 {
     return this->width;
 }
 
-template <typename T, typename T_point, typename T_compute>
-T Rectangle<T, T_point, T_compute>::getHeight() const
+template <typename T_point, typename T_compute>
+T_compute Rectangle<T_point, T_compute>::getHeight() const
 {
     return this->height;
 }
 
-template <typename T, typename T_point, typename T_compute>
-T_compute Rectangle<T, T_point, T_compute>::perimeter()
+template <typename T_point, typename T_compute>
+T_compute Rectangle<T_point, T_compute>::perimeter()
 {
     return static_cast<T_compute> (2 * this->width + 2 * this->height);
 }
 
-template <typename T, typename T_point, typename T_compute>
-T_compute Rectangle<T, T_point, T_compute>::surface()
+template <typename T_point, typename T_compute>
+T_compute Rectangle<T_point, T_compute>::surface()
 {
     return static_cast<T_compute> (this->width * this->height);
 }
 
-template <typename T, typename T_point, typename T_compute>
-std::ostream &operator<<(std::ostream &cout, Rectangle<T, T_point, T_compute> const& rectangle)
+template <typename T_point, typename T_compute>
+std::ostream &operator<<(std::ostream &cout, Rectangle<T_point, T_compute> const& rectangle)
 {
     cout << "Rectangle(width: " << rectangle.width << ", height: " << rectangle.height << ", center: " << rectangle.center << ")";
     return cout;
--- include/Square.hpp
+++ include/Square.hpp
@@ -2,31 +2,31 @@
 
 #include "Rectangle.hpp"
 
-template <typename T, typename T_point = T, typename T_compute = T_point>
+template <typename T_point, typename T_compute = T_point>
 class Square;
 
-template <typename T, typename T_point = T, typename T_compute = T_point>
-std::ostream &operator<<(std::ostream &cout, Square<T, T_point, T_compute> const &square);
+template <typename T_point, typename T_compute = T_point>
+std::ostream &operator<<(std::ostream &cout, Square<T_point, T_compute> const &square);
 
-template <typename T, typename T_point, typename T_compute>
-class Square : public Rectangle<T, T_point, T_compute>
+template <typename T_point, typename T_compute>
+class Square : public Rectangle<T_point, T_compute>
 {
 public:
-    Square(T size, T_point x, T_point y);
+    Square(T_compute size, T_point x, T_point y);
     ~Square();
 
-    friend std::ostream &operator<< <T, T_point, T_compute>(std::ostream &cout, Square const &square);
+    friend std::ostream &operator<< <T_point, T_compute>(std::ostream &cout, Square const &square);
 };
 
-template <typename T, typename T_point, typename T_compute>
-Square<T, T_point, T_compute>::Square(T size, T_point x, T_point y)
-: Rectangle<T, T_point, T_compute>(size, size, x, y) {}
+template <typename T_point, typename T_compute>
+Square<T_point, T_compute>::Square(T_compute size, T_point x, T_point y)
+: Rectangle<T_point, T_compute>(size, size, x, y) {}
 
-template <typename T, typename T_point, typename T_compute>
-Square<T, T_point, T_compute>::~Square() {}
+template <typename T_point, typename T_compute>
+Square<T_point, T_compute>::~Square() {}
 
-template <typename T, typename T_point, typename T_compute>
-std::ostream &operator<<(std::ostream &cout, Square<T, T_point, T_compute> const &square)
+template <typename T_point, typename T_compute>
+std::ostream &operator<<(std::ostream &cout, Square<T_point, T_compute> const &square)
 {
     cout << "Square(width: " << square.width << ", center: " << square.center << ")";
     return cout;
